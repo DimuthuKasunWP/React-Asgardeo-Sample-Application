419066256c04bf897d88a14c0f508e3c3ad1dadb
DimuthuKasunWP <dimuthuk@wso2.com>
2021-11-12 23:29:27 +0530

    Adding minor change

--- src/data/Data.tsx
+++ src/data/Data.tsx
@@ -2,7 +2,7 @@ export const JsonData = {
 
     "Header": {
         "title": "React SPA With Asgardeo Authentication",
-        "paragraph": "Sample Demo Application With Asgardeo User Authentication. You Can Use this as Single Landing Page Application"
+        "paragraph": "Sample Demo Application With Asgardeo User Authentication. You Can Use This As a Single Page Application"
     },
     "About": {
         "paragraph": "Asgardeo is an IDaaS empowering developers to implement secure and frictionless access. Here’s our Early Adopter release - Start your journey today for free!",
